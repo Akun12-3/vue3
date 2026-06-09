2d74a04ef0160bed82a38d32675318684f21a61f
aliyun8473049997 <2505075327@qq.com>
2026-06-09 19:36:56 +0800

    init

---
+++ script/dev.js
@@ -0,0 +1,23 @@
+import esbuild  from 'esbuild'
+import { resolve } from 'path'
+import  minimist  from 'minimist'
+import { fileURLToPath } from 'url'
+import { createRequire } from 'module'
+const argv = minimist(process.argv.slice(2))
+const __dirname = fileURLToPath(import.meta.url)
+const target = argv._[0] || 'reactivity'
+const format = argv.f ? argv.f : 'iife'
+const require = createRequire(import.meta.url)
+// 获取打包格式
+
+const pkg =  require(`../packages/${target}/src/package.json`)
+const entry = resolve(__dirname,`../packages/${target}/src/index.ts`)
+await esbuild.context({
+    entryPoints:[entry],
+    outfile:resolve(__dirname,`../packages/dist/${target}.js`),
+    bundle:true, //reactivity->shard 会打包到一起
+    platform:'browser',
+    sourcemap:true,
+    format,
+    globalName:pkg.buildOptions.nama
+})
